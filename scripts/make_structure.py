feea083d199a656ed5f204080c712badcd06338c
Ariane Mora <ariane.n.mora@gmail.com>
2025-07-07 11:15:12 -0700

    Updated structures

--- scripts/make_structure.py
+++ scripts/make_structure.py
@@ -7,13 +7,17 @@ os.environ['MKL_THREADING_LAYER'] = 'GNU'
 import time
 start = time.time()
 
-output_dir = 'boltz_26062025/'
+output_dir = 'boltz_28062025/'
 num_threads = 5
 id_col = 'name'
 seq_col = 'aa_sequence'
+seq_col = 'AA'
+
 substrate_col = 'Substrate'
 intermediate_col = 'Intermediate'
-df = pd.read_csv('../output/LevSeq_unique_variants_todo.csv')
+#df = pd.read_csv('../output/LevSeq_unique_variants_todo.csv')
+df = pd.read_csv('../output/lineage_df.csv')
+
 df[substrate_col] = "O=C(OC)CCC1=CC=CC=C1"
 df[intermediate_col] = str(r"CC1=C(/C2=C/C3=N/C(C(C)=C3CCC(O)=O)=C\C4=C(C(C=C)=C(/C=C5N=C(C(C=C)=C\5C)/C=C1\N26)N4[Fe]6=N)C)CCC(O)=O")
 
